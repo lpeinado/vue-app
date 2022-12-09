e9546809d5ab43969887a5740ecf78df42c16da2
Lluis Peinado <lpeinado@stelladot.com>
2022-12-09 12:23:40 +0100

    master: adding a simple title composable

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
+  <input type="text" v-model="title">
   <ToDo />
 </template>
 
 <script>
 import ToDo from './components/ToDo.vue'
+import {useTitle} from '@vueuse/core'
 
 export default {
   name: 'App',
+  setup () {
+    const title = useTitle("Initial")
+    return {title}
+  },
   components: {
     ToDo
   }
