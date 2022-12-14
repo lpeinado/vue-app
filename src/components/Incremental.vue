b247035f3695bde066fe86fc1b22ac14d5385dc0
Lluis Peinado <lpeinado@stelladot.com>
2022-12-14 18:09:54 +0100

    master: adding lifecycle hooks for components to console log themselves

--- src/components/Incremental.vue
+++ src/components/Incremental.vue
@@ -11,10 +11,49 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  onActivated,
+  onDeactivated,
+  onErrorCaptured
+} from "vue";
 import myModule from '../modules/myModule'
 
 export default defineComponent({
   setup () {
+    onBeforeMount(() => {
+      console.log("Before Mount!");
+    });
+    onMounted(() => {
+      console.log("Mounted!");
+    });
+    onBeforeUpdate(() => {
+      console.log("Before Update!");
+    });
+    onUpdated(() => {
+      console.log("Updated!");
+    });
+    onBeforeUnmount(() => {
+      console.log("Before Unmount!");
+    });
+    onUnmounted(() => {
+      console.log("Unmounted!");
+    });
+    onActivated(() => {
+      console.log("Activated!");
+    });
+    onDeactivated(() => {
+      console.log("Deactivated!");
+    });
+    onErrorCaptured(() => {
+      console.log("Error Captured!");
+    });
+    
     const {counter, increment, placesLeft, attending} = myModule();
     return {counter, increment, placesLeft, attending}
   }
